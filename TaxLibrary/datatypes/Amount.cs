e5bf06e13f15015a3a50de0730eeaaa75e857f12
Luka63-17 <lukanrt6317@gs.viser.edu.rs>
2020-11-16 11:58:57 +0100

    Added openJDK.BigMathNet library Amount and DecimalNumber classes

---
+++ TaxLibrary/datatypes/Amount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using java.math;
+
+namespace TaxLibrary.datatypes
+{
+    class Amount : DecimalNumber
+    {
+        private static long serialVersionUID = 1L;
+        private static int NUMBER_OF_AMOUNT_DECIMALS = 2;
+        private static readonly BigDecimal ZERO_VALUE = (new Amount(DecimalNumber.ZERO)).getValue();
+
+        public Amount(BigDecimal value) : base(NUMBER_OF_AMOUNT_DECIMALS)
+        {
+            setValue(value);
+        }
+
+        public Amount(double value) : this(BigDecimal.valueOf(value))
+        {
+        }
+
+        public void add(Amount otherValue)
+        {
+            setValue(getValue().add(otherValue.getValue()));
+        }
+
+        public void subtract(Amount otherValue)
+        {
+            setValue(getValue().subtract(otherValue.getValue()));
+        }
+    }
+}
---
+++ TaxLibrary/datatypes/DecimalNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using java.math;
+
+namespace TaxLibrary.datatypes
+{
+    class DecimalNumber
+    {
+        protected static readonly BigDecimal ZERO = java.math.BigDecimal.ZERO;
+        protected static readonly RoundingMode DEFAULT_ROUNDING_MODE = RoundingMode.HALF_EVEN;
+
+        private BigDecimal value;
+        private int decimals;
+
+        public DecimalNumber(int decimals)
+        {
+            setPrecision(decimals);
+            setValue(ZERO);
+        }
+
+        public DecimalNumber(int decimals, BigDecimal value) : this(decimals)
+        {
+            setValue(value);
+        }
+
+        // Nisam siguran sta ovde treba uopste
+        //public final BigDecimal protected static BigDecimal ZERO => zERO;
+
+        public BigDecimal getValue()
+        {
+            return value;
+        }
+
+        private void setPrecision(int decimals)
+        {
+            this.decimals = decimals;
+        }
+
+        public int getPrecision()
+        {
+            return decimals;
+        }
+
+        public void setValue(BigDecimal value)
+        {
+            this.value = value.setScale(decimals, DEFAULT_ROUNDING_MODE);
+        }
+
+        public void add(DecimalNumber otherValue)
+        {
+            this.value = value.add(otherValue.getValue()).setScale(decimals, DEFAULT_ROUNDING_MODE);
+        }
+
+        public void subtract(DecimalNumber otherValue)
+        {
+            this.value = value.subtract(otherValue.getValue()).setScale(decimals, DEFAULT_ROUNDING_MODE);
+        }
+
+        public void multiply(Int32 otherValue)
+        {
+            this.value = value.multiply(new BigDecimal(otherValue)).setScale(decimals, DEFAULT_ROUNDING_MODE);
+        }
+
+
+        public override String ToString()
+        {
+            return value.toPlainString();
+        }
+    }
+}
